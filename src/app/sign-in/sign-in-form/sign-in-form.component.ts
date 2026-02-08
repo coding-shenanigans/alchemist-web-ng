d1958a9b372d577eda9565d9f7f2a7b9d5821805
Coding Shenanigans <94819102+coding-shenanigans@users.noreply.github.com>
2026-02-08 01:08:14 -0800

    Integrate with the sign in API (#4)

--- src/app/sign-in/sign-in-form/sign-in-form.component.ts
+++ src/app/sign-in/sign-in-form/sign-in-form.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
-import { RouterLink } from '@angular/router';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
+import { AuthApi } from '../../auth-api';
+import { finalize } from 'rxjs';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @Component({
   selector: 'app-sign-in-form',
@@ -15,15 +18,23 @@ import { RouterLink } from '@angular/router';
     MatFormFieldModule,
     ReactiveFormsModule,
     RouterLink,
+    MatProgressSpinnerModule,
   ],
   templateUrl: './sign-in-form.component.html',
   styleUrl: './sign-in-form.component.scss',
 })
 export class SignInForm {
-  signInForm = new FormGroup({
-    email: new FormControl(''),
-    password: new FormControl(''),
-    showPassword: new FormControl(false),
+  private formBuilder = inject(FormBuilder).nonNullable;
+  private router = inject(Router);
+  private authApi = inject(AuthApi);
+
+  isLoading = signal(false);
+  errorMessages = signal<string[]>([]);
+
+  signInForm = this.formBuilder.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required]],
+    showPassword: [false],
   });
 
   toggleShowPassword(event: MouseEvent) {
@@ -32,7 +43,29 @@ export class SignInForm {
     this.signInForm.controls.showPassword.setValue(!currentValue);
   }
 
-  onSubmit() {
-    console.log(this.signInForm.value);
+  handleSubmit() {
+    if (this.signInForm.invalid) {
+      return;
+    }
+
+    this.isLoading.set(true);
+    this.errorMessages.set([]);
+
+    const { email, password } = this.signInForm.getRawValue();
+
+    this.authApi
+      .signIn(email, password)
+      .pipe(finalize(() => this.isLoading.set(false)))
+      .subscribe({
+        next: (data) => {
+          // TODO: set user in app state
+          console.log('Signed in successfully:', data);
+          this.router.navigate(['/']);
+        },
+        error: (error) => {
+          console.error('Failed to sign in:', error);
+          this.errorMessages.set(error.error?.errors ?? ['unknown error']);
+        },
+      });
   }
 }
