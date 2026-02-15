a5a9a602d2a9dd8acc21048fdfdf5f6070bde18e
Coding Shenanigans <94819102+coding-shenanigans@users.noreply.github.com>
2026-02-14 23:09:06 -0800

    Add user menu (#6)

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -4,10 +4,12 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterLink } from '@angular/router';
 import { AuthApi } from '../auth-api';
 import { AsyncPipe } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
 
 @Component({
   selector: 'app-header',
-  imports: [MatButtonModule, MatToolbarModule, RouterLink, AsyncPipe],
+  imports: [MatButtonModule, MatToolbarModule, RouterLink, AsyncPipe, MatIconModule, MatMenuModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
